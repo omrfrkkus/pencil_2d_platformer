f67d082d111052214cda66ed0b34983894f6265d
Ömer Faruk Kuş <omrfrkk@yahoo.com>
2024-02-02 21:02:19 +0100

    added mobile controls
    Mobile controls and buttons added

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -12,8 +12,8 @@ func _process(delta):
 
 
 func _on_level_1_pressed():
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
 
 
 func _on_level_2_pressed():
-	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/level2.tscn")
