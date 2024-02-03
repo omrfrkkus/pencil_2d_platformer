2e71f2e53cd9914ae4944e68640b61e803e5e4d9
Ömer Faruk Kuş <omrfrkk@yahoo.com>
2024-02-03 23:41:30 +0100

    pause menu updated

--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -1,12 +1,11 @@
 extends Control
 
 @export var game_manager : GameManager
+@export var transitioner : Transitioner
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
 	game_manager.connect("toggle_game_paused", _on_game_manager_toogle_game_paused)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func _on_game_manager_toogle_game_paused(is_paused : bool):
 	if(is_paused):
@@ -14,10 +13,16 @@ func _on_game_manager_toogle_game_paused(is_paused : bool):
 	else:
 		hide()
 
-
 func _on_resume_button_pressed():
 	game_manager.game_paused = false
 
-
 func _on_restart_button_pressed():
-	pass # Replace with function body.
+	hide()
+	transitioner.set_reload_animation(true)
+
+func _on_close_button_pressed():
+	hide()
+	transitioner.set_exit_animation(true)
+
+func _on_sound_button_pressed():
+	pass
--- UI/transitioner.gd
+++ UI/transitioner.gd
@@ -9,6 +9,7 @@ class_name  Transitioner
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 var is_reload : bool = false
+var is_exit : bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_tex.visible = false
@@ -28,11 +29,21 @@ func set_reload_animation(fading_out : bool):
 		animation_player.queue("fade_out")
 	else:
 		animation_player.queue("fade_in")
+		
+func set_exit_animation(fading_out : bool):
+	get_tree().paused = true
+	is_exit = true
+	if(fading_out):
+		animation_player.queue("fade_out")
+	else:
+		animation_player.queue("fade_in")
 
 
 func _on_animation_player_animation_finished(anim_name):
 	get_tree().paused = false
 	if(is_reload):
 		get_tree().reload_current_scene()
+	elif(is_exit):
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	elif(scene_to_load != null && anim_name == scene_switch_anim):
 		get_tree().change_scene_to_packed(scene_to_load)
