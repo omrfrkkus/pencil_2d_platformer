5335f290041a909c365ab7914c131c5455f42466
Ömer Faruk Kuş <omrfrkk@yahoo.com>
2024-02-08 00:41:19 +0100

    level selector added

--- scenes/menus/level_selector.gd
+++ scenes/menus/level_selector.gd
@@ -6,9 +6,22 @@ func _ready():
 	pass # Replace with function body.
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
 func _on_level_1_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
 
 
 func _on_level_2_pressed():
 	get_tree().change_scene_to_file("res://scenes/levels/level2.tscn")
+
+
+func _on_level_3_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level3.tscn")
+
+
+func _on_level_4_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level4.tscn")
